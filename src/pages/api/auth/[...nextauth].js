f25bd41dd6713b8daf133a00ff61593b38da6951
AmmarHammamieh <72897926+AmmarHammamieh@users.noreply.github.com>
2022-03-16 02:33:46 +0200

    Update [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import jwt from "next-auth/jwt"
 
 export default NextAuth({
   // Configure one or more authentication providers
+  const secret = process.env.JWT_SECRET
   providers: [
     GoogleProvider({
       clientId: process.env.Google_ID,
